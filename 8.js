58302bd75598ac8d590f3717e400f828b1105127
JCamro <cameroelguerajosue@gmail.com>
2025-10-27 17:15:44 -0500

    ejercicio 8

---
+++ 8.js
@@ -0,0 +1,6 @@
+const convertirAMayusculas = function (cadena) {
+    return cadena.toUpperCase();
+} 
+
+console.log(convertirAMayusculas("hola mundo")); // "HOLA MUNDO"
+console.log(convertirAMayusculas("JavaScript es genial")); // "JAVASCRIPT ES GENIAL"
